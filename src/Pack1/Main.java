c235c3b8f1cab95905db3166dafffe004f952575
Pascal Sore <pascal.sore987@gmail.com>
2024-03-14 14:37:51 -1000

    Ceci est la version 5

--- src/Pack1/Main.java
+++ src/Pack1/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("C'est la Version 1");
 		System.out.println("C'est la Version 2");
+		System.out.println("C'est la Version 5");
 	}
 
 }
