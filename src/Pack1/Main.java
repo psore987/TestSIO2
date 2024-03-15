d9772362abd15609963ebc823ab8bf17cc65eb0d
Pascal Sore <pascal.sore987@gmail.com>
2024-03-14 14:25:30 -1000

    Ceci est la version 2

---
+++ src/Pack1/Main.java
@@ -0,0 +1,10 @@
+package Pack1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("C'est la Version 1");
+		System.out.println("C'est la Version 2");
+	}
+
+}
